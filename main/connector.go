eeb2b6b6788ed3f4750359509305c16c5a3f0560
huyhoang294 <44403225+huyhoang294@users.noreply.github.com>
2023-04-15 03:26:14 +0800

    first commit

---
+++ main/connector.go
@@ -0,0 +1,42 @@
+components {
+  id: "connector"
+  component: "/main/connector.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "tile_set: \"/main/assets.atlas\"\n"
+  "default_animation: \"selector\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  "size {\n"
+  "  x: 500.0\n"
+  "  y: 500.0\n"
+  "  z: 0.0\n"
+  "  w: 0.0\n"
+  "}\n"
+  "size_mode: SIZE_MODE_MANUAL\n"
+  ""
+  position {
+    x: -40.0
+    y: 50.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
