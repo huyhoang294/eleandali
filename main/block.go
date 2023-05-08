2ce6055f04d520f941963d6e0cbed6b961455f83
huyhoang294 <44403225+huyhoang294@users.noreply.github.com>
2023-05-09 02:48:57 +0800

    Update

--- main/block.go
+++ main/block.go
@@ -17,7 +17,7 @@ embedded_components {
   id: "sprite"
   type: "sprite"
   data: "tile_set: \"/main/assets.atlas\"\n"
-  "default_animation: \"blue-orb-1\"\n"
+  "default_animation: \"orb-blue\"\n"
   "material: \"/builtins/materials/sprite.material\"\n"
   "blend_mode: BLEND_MODE_ALPHA\n"
   ""
---
+++ main/specialBlock.go
@@ -0,0 +1,59 @@
+embedded_components {
+  id: "magic"
+  type: "sprite"
+  data: "tile_set: \"/main/assets.atlas\"\n"
+  "default_animation: \"blue-orb-2\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  "size {\n"
+  "  x: 829.0\n"
+  "  y: 827.0\n"
+  "  z: 0.0\n"
+  "  w: 0.0\n"
+  "}\n"
+  "size_mode: SIZE_MODE_MANUAL\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.001
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "light"
+  type: "sprite"
+  data: "tile_set: \"/main/assets.atlas\"\n"
+  "default_animation: \"magic-sphere_layer3\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  "size {\n"
+  "  x: 829.0\n"
+  "  y: 827.0\n"
+  "  z: 0.0\n"
+  "  w: 0.0\n"
+  "}\n"
+  "size_mode: SIZE_MODE_MANUAL\n"
+  ""
+  position {
+    x: 11.0
+    y: -37.0
+    z: 0.002
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+  scale {
+    x: 1.24
+    y: 1.079
+    z: 1.0
+  }
+}
